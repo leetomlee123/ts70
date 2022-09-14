e16f571c9d1fa397af10a735d61fa4188ada700e
leetomlee123 <leetomlee123@gmail.com>
2022-09-14 21:20:36 +0800

    init

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 call flutter clean
-call flutter build apk --obfuscate --split-debug-info=HLQ_Struggle --target-platform android-arm64 --split-per-abi --build-name=0.0.3 --build-number=3
+call flutter build apk --obfuscate --split-debug-info=HLQ_Struggle --target-platform android-arm64 --split-per-abi --build-name=0.0.4 --build-number=3
 call cd build/app/outputs/apk/release
 call ipconfig
 call python -m http.server 80
